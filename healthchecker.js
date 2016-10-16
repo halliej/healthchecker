6bb7dff7b0c0aab21cffca92c1541abbf29285da
Hal DeLage <haldelage@gmail.com>
2016-10-16 08:04:32 -0400

    Added comments and organized declarations.

--- healthchecker.js
+++ healthchecker.js
@@ -3,18 +3,25 @@ const cp = require("child_process");
 const process = require('process');
 const util = require('util');
 const winston = require('winston');
-
 const fs = require('fs');
 const env = process.env.NODE_ENV || 'development';
 const logDir = 'logs';
+
+let testInterval = 30000;   //how often test will be run
+let statusPort = 3000;      //webserver is listening on this port
+let status = '{"passed":0,"failed":0,"msgs":[]}';   //default status
+
 // Create the log directory if it does not exist
 if (!fs.existsSync(logDir)) {
   fs.mkdirSync(logDir);
 }
 
+//format for log timestamp
 const tsFormat = () => (new Date()).toLocaleTimeString('en-US',{timeZone:'America/New_York'}); 
+//format for log messages
 const msgFormat = (options) => (options.timestamp() +' '+ options.level.toUpperCase() +' '+ (undefined !== options.message ? options.message : '') +
           (options.meta && Object.keys(options.meta).length ? '\n\t'+ JSON.stringify(options.meta) : ''));
+//create logger with console and 2 file transports
 const logger = new (winston.Logger)({
   transports: [
     new (winston.transports.Console)({
@@ -42,10 +49,7 @@ const logger = new (winston.Logger)({
   ]
 });
 
-let testInterval = 30000;
-let statusPort = 3000;
-let status = '{"passed":0,"failed":0,"msgs":[]}';
-
+//start doing stuff
 logger.verbose('Automatic test running...');
 logger.verbose(`Status will be reported on port ${statusPort}`);
 
