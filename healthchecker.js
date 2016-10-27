474caf43b0e749d09d3a5fb59bac58b8a94a6a62
Hal DeLage <haldelage@gmail.com>
2016-10-27 14:44:40 -0400

    Moved logger stuff to new module.

--- healthchecker.js
+++ healthchecker.js
@@ -2,52 +2,13 @@ const http = require('http');
 const cp = require("child_process");
 const process = require('process');
 const util = require('util');
-const winston = require('winston');
-const fs = require('fs');
+const logger = require('./logs.js');
+
 const env = process.env.NODE_ENV || 'development';
-const logDir = 'logs';
 
 let testInterval = 30000;   //how often test will be run
 let statusPort = 3000;      //webserver is listening on this port
 let status = '{"passed":0,"failed":0,"msgs":[]}';   //default status
-
-// Create the log directory if it does not exist
-if (!fs.existsSync(logDir)) {
-  fs.mkdirSync(logDir);
-}
-
-//format for log timestamp
-const tsFormat = () => (new Date()).toLocaleTimeString('en-US',{timeZone:'America/New_York'}); 
-//format for log messages
-const msgFormat = (options) => (options.timestamp() +' '+ options.level.toUpperCase() +' '+ (undefined !== options.message ? options.message : '') +
-          (options.meta && Object.keys(options.meta).length ? '\n\t'+ JSON.stringify(options.meta) : ''));
-//create logger with console and 2 file transports
-const logger = new (winston.Logger)({
-  transports: [
-    new (winston.transports.Console)({
-        timestamp: tsFormat,
-        colorize: true,
-        level: 'debug',
-        formatter: msgFormat
-    }),
-    new (require('winston-daily-rotate-file'))({
-      filename: `${logDir}/-teststatus.log`,
-      timestamp: tsFormat,
-      formatter: msgFormat,
-      level: 'info',
-      datePattern: 'yyyy-MM-dd',
-      prepend: true,
-      json: false
-    }),
-    new (winston.transports.File)({
-      name: 'error-file',
-      filename: `${logDir}/error.log`,
-      level: 'error',
-      formatter: msgFormat,
-      json: false
-    })
-  ]
-});
 
 //start doing stuff
 logger.verbose('Automatic test running...');
@@ -78,9 +39,3 @@ http.createServer(function (request, response) {
    response.writeHead(200, {'Content-Type': 'text/json'});
    response.end(status);
 }).listen(statusPort);
-
-
-
-
-
-
