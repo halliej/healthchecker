89fa5d2793dd444a7b419ddd0f6e08ecb822ef90
Hal DeLage <haldelage@gmail.com>
2016-10-27 14:44:17 -0400

    Initial commit.

---
+++ logs.js
@@ -0,0 +1,43 @@
+const winston = require('winston');
+const fs = require('fs');
+const logDir = 'logs';
+
+// Create the log directory if it does not exist
+if (!fs.existsSync(logDir)) {
+  fs.mkdirSync(logDir);
+}
+
+//format for log timestamp
+const tsFormat = () => (new Date()).toLocaleTimeString('en-US',{timeZone:'America/New_York'}); 
+//format for log messages
+const msgFormat = (options) => (options.timestamp() +' '+ options.level.toUpperCase() +' '+ (undefined !== options.message ? options.message : '') +
+          (options.meta && Object.keys(options.meta).length ? '\n\t'+ JSON.stringify(options.meta) : ''));
+//create logger with console and 2 file transports
+const logger = new (winston.Logger)({
+  transports: [
+    new (winston.transports.Console)({
+        timestamp: tsFormat,
+        colorize: true,
+        level: 'debug',
+        formatter: msgFormat
+    }),
+    new (require('winston-daily-rotate-file'))({
+      filename: `${logDir}/-teststatus.log`,
+      timestamp: tsFormat,
+      formatter: msgFormat,
+      level: 'info',
+      datePattern: 'yyyy-MM-dd',
+      prepend: true,
+      json: false
+    }),
+    new (winston.transports.File)({
+      name: 'error-file',
+      filename: `${logDir}/error.log`,
+      level: 'error',
+      formatter: msgFormat,
+      json: false
+    })
+  ]
+});
+
+module.exports = logger;
